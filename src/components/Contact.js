8db8b26742dc00a879e4664a126a973808702401
İpek Ülfet Kaylan <ipekulfetkaylan@gmail.com>
2023-03-21 21:32:19 +0300

    about ve contact eklendi

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import bannerImage from "../assets/contactImage.jpg";
+import "../styles/Contact.css";
 
 function Contact() {
   return (
-    <div>
-      contact
+    <div className='contact'>
+      <div className='leftSide' style={{backgroundImage:`url(${bannerImage})`}}></div>
+      <div className='rightSide'>
+        <h1>Bizimle İletişime Geçin</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input type="text" placeholder='Ad ve soyad giriniz'/>
+          <label>Email</label>
+          <input type="email" placeholder='Email giriniz'/>
+          <label>Mesajınız</label>
+          <textarea rows="6" placeholder='Mesajınız'></textarea>
+          <button type='submit'>Gönder</button>
+        </form>
+      </div>
+      
     </div>
   )
 }
